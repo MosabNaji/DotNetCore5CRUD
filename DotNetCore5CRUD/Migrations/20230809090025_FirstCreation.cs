1d00a6cd4bbf4a0f7b1f5f0c2a036ddb08b6f924
Mosab Naji <MosabNaji2002@Gmail.com>
2023-08-09 13:19:40 +0300

    Add project files.

---
+++ DotNetCore5CRUD/Migrations/20230809090025_FirstCreation.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DotNetCore5CRUD.Migrations
+{
+    public partial class FirstCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
